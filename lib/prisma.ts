260281ebaf0a89262e1403d96b7986eadf7b87e0
vinceflores <vincegabriel.flores@gmail.com>
2024-11-03 16:43:24 -0500

    twotier

--- lib/prisma.ts
+++
@@ -1,6 +0,0 @@
-import { PrismaClient } from "@prisma/client/react-native";
-import { reactiveQueriesExtension } from "@prisma/react-native";
-
-const baseClient = new PrismaClient();
-
-export const extendedClient = baseClient.$extends(reactiveQueriesExtension());
