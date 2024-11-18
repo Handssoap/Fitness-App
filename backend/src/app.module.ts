f6074ecf97d11721812e35a451f75100f1c47013
vinceflores <vincegabriel.flores@gmail.com>
2024-11-17 19:13:23 -0500

    remove exercise resource

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,16 +3,14 @@ import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { WorkoutModule } from './workout/workout.module';
-import { ExerciseModule } from './exercise/exercise.module';
 import { ClerkModule } from './clerk/clerk.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true, }),
+    ConfigModule.forRoot({ isGlobal: true }),
     PrismaModule,
     WorkoutModule,
-    ExerciseModule,
     ClerkModule,
   ],
   controllers: [],
